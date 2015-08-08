bc6b21ee00476ba23eb07a5ea54f30db529d3e70
Mike Chandler <mike@rmwebfx.com>
2015-08-08 09:00:07 -0700

    Adding to Github

---
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -0,0 +1,29 @@
+package com.zonkware.pi4led.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
+    }
+
+    @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
+    }
+
+}
