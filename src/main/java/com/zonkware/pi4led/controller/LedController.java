c1b61a8c05aebdafbfa47396c3ff4798e14ef9ec
Mike Chandler <mike@rmwebfx.com>
2015-08-08 10:31:44 -0700

    Changes to the Controller

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -4,6 +4,8 @@ import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class LedController {
 
@@ -14,16 +16,52 @@ public class LedController {
         return "Hello world!";
     }
 
-    @RequestMapping("/light")
-    public String light() {
+    @RequestMapping("/toggle")
+    public String toggle() {
+        getPin().toggle();
+
+        return "OK";
+    }
+
+    private String checkState() {
+        return (getPin().isHigh() ? "Light is ON" : "Light is OFF");
+    }
+
+    @RequestMapping("/on")
+    public String on() {
+        getPin().high();
+
+        return checkState();
+    }
+
+    @RequestMapping("/off")
+    public String off() {
+        getPin().low();
+
+        return checkState();
+    }
+
+    @RequestMapping("/blink")
+    public String blink() {
+        getPin().blink(200L, 5000L);
+
+        return "Light is blinking...";
+    }
+
+    @RequestMapping("/pulse")
+    public String pulse() {
+        getPin().pulse(5000L);
+
+        return "Light is pulsing...";
+    }
+
+    public GpioPinDigitalOutput getPin() {
         if (pin == null) {
             GpioController gpio = GpioFactory.getInstance();
             pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
         }
 
-        pin.toggle();
-
-        return "OK";
+        return pin;
     }
 
 }
